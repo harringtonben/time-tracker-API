2f77cec4ed3a08a88b9a042f87d689add9f4d78d
ben harrington <harringtonben@icloud.com>
2018-05-29 20:51:43 -0500

    finally connected to azure database

---
+++ time-tracker-API/Controllers/SupporterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using time_tracker_API.Services;
+
+namespace time_tracker_API.Controllers
+{
+    [Route("api/supporters")]
+    public class SupporterController : Controller
+    {
+        private readonly SupporterRepository _repo;
+
+        public SupporterController(SupporterRepository repo)
+        {
+            _repo = repo;
+        }
+        
+        public IActionResult GetAll()
+        {
+            var allSupporters = _repo.GetAllSupporters();
+
+            return Ok(allSupporters);
+        }
+    }
+}
