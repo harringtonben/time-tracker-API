2c9702a530d4ac0e6b3b6df65b8fbe8298c91622
ben harrington <harringtonben@icloud.com>
2018-06-02 12:52:05 -0500

    finished injecting connection string into new controller and repository

---
+++ time-tracker-API/Controllers/ShiftController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using time_tracker_API.Services;
+
+namespace time_tracker_API.Controllers
+{   
+    [Route("api/shifts")]
+    public class ShiftController : Controller
+    {
+        private readonly ShiftRepository _repo;
+
+        public ShiftController(ShiftRepository repo)
+        {
+            _repo = repo;
+        }
+
+       
+    }
+}
