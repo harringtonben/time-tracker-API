b23c8c5c62c1acaf448f5a35a8993b12268b51e0
ben harrington <harringtonben@icloud.com>
2018-06-04 20:40:11 -0500

    forgot to move metric class out of repository

--- time-tracker-API/Services/SupporterRepository.cs
+++ time-tracker-API/Services/SupporterRepository.cs
@@ -135,14 +135,5 @@ namespace time_tracker_API.Services
             }
         }
     }
-
-    public class SupporterMetric
-    {
-        public int EmployeeId { get; set; }
-        public string Name { get; set; }
-        public int? TotalWorkedFromHome { get; set; }
-        public int? TotalCalledOut { get; set; }
-        public int? TotalUnplannedOut { get; set; }
-    }
 }
 
