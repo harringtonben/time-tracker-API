c3fdbbc114c0a49a7d871f537655a1c4a52acc57
ben harrington <harringtonben@icloud.com>
2018-06-05 20:45:45 -0500

    finished csv export

---
+++ time-tracker-API/Models/ReportMetrics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace time_tracker_API.Services
+{
+    public class ReportMetrics
+    {
+        public int ShiftId { get; set; }
+        public DateTime Date { get; set; }
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public bool WorkFromHome { get; set; }
+        public bool Callout { get; set; }
+        public bool Planned { get; set; }
+        public bool Phone { get; set; }
+        public bool Email { get; set; }
+        public bool Integrations { get; set; }
+        public bool NonCoverage { get; set; }
+        public int? UnplannedSickTime { get; set; }
+        public int? TotalPhoneDays { get; set; }
+        public int? TotalEmailDays { get; set; }
+    }
+}
---
+++ time-tracker-API/Models/Reports.cs
@@ -0,0 +1,15 @@
+﻿namespace time_tracker_API.Controllers
+{
+    public enum Reports
+    {
+        AllShiftsWithinXWeeks=1,
+        WorkfromHomeWithinXWeeks,
+        UnplannedCalloutsWithinXWeeks,
+        AllShiftsByManagerWithinXWeeks,
+        UnplannedVsPlannedSickDaysWithinXWeeks,
+        AllEmailDays,
+        AllPhoneDays,
+        TotalPhoneDays,
+        TotalEmailDays
+    }
+}
